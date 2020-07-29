167a0a1f1bf265083b2b8ce57422bad5d1070dd3
miriamela <grossi.miriam@gmail.com>
2020-07-29 12:01:38 +0200

    create docs folder

---
+++ docs/precache-manifest.4a58dab657a2cb6368ac629b58f47601.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90aa1f7e177b83a1a403c9c6008b3924",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a260182974df01c0fde",
+    "url": "./static/css/main.963266a9.chunk.css"
+  },
+  {
+    "revision": "a6c124c8eddbe9eb5e9b",
+    "url": "./static/js/2.55824ddc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.55824ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a260182974df01c0fde",
+    "url": "./static/js/main.1786e526.chunk.js"
+  },
+  {
+    "revision": "5a6c26ab9c21e9660958",
+    "url": "./static/js/runtime-main.d4720b12.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logo-awesome-profile-cards.0118e311.svg"
+  },
+  {
+    "revision": "ee467a1e52c5f1148800e2d35c394c28",
+    "url": "./static/media/logo_futuramas.ee467a1e.png"
+  },
+  {
+    "revision": "a209326f89ed8832c6e4715dc1b8c75e",
+    "url": "./static/media/profile-image.a209326f.jpg"
+  }
+]);
